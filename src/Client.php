079b9cc4f01e284f9f532408b6f93d94c6d124b5
reserved <reserved@mail.ru>
2022-02-21 21:27:53 +0300

    Debug stuff

--- src/Client.php
+++ src/Client.php
@@ -985,7 +985,7 @@ class Client
      *
      * @return self
      */
-    protected function setDebug(bool $debug)
+    public function setDebug(bool $debug)
     {
         $this->debug = $debug;
 
